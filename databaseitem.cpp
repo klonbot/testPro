e24c03f19d2413bcfc0c433509326f0754245751
Dmitriy <dn.rybakov@ddkmed.ru>
2019-08-06 19:24:07 +0700

    Описал создание новых записей

--- databaseitem.cpp
+++ databaseitem.cpp
@@ -1,6 +1,22 @@
 #include "databaseitem.h"
 
-DataBaseItem::DataBaseItem()
+DataBaseItem::DataBaseItem(idDataBaseItem_t id, idDataBaseItem_t idP)
 {
+    init(id, idP, isRoot_false);
+}
 
+DataBaseItem::DataBaseItem(idDataBaseItem_t id) // для создания корня
+{
+    idDataBaseItem_t idP = 0;
+    init(id, idP, isRoot_true);
+}
+
+void DataBaseItem::init(idDataBaseItem_t id, idDataBaseItem_t idP, isRoot_t isR)
+{
+    isRoot = isR;
+    idThis = id;
+    idParent = idP;
+    idChildren.clear();
+    isDeleted = isDeleted_false;
+    value.clear();
 }
--- databaseitem.h
+++ databaseitem.h
@@ -2,19 +2,30 @@
 #define DATABASEITEM_H
 
 #include "QString"
+#include "QVector"
+
+typedef enum {isRoot_false, isRoot_true} isRoot_t;
+typedef enum {isDeleted_false, isDeleted_true} isDeleted_t;
+typedef unsigned int idDataBaseItem_t;
 
 class DataBaseItem
 {
 public:
-    DataBaseItem();
+    DataBaseItem(idDataBaseItem_t id, idDataBaseItem_t idP);
+    DataBaseItem(idDataBaseItem_t id);
 
 private:
-     bool isRoot;
-     bool isDelited;
+    idDataBaseItem_t idThis;
 
-     QVector<DataBaseItem> children;    // список дочерних элементов
-     DataBaseItem *Parent;              // Ссылка на родителя
-     QString value;
+    QString value;
+
+    isRoot_t isRoot;
+    isDeleted_t isDeleted;
+
+    QVector<idDataBaseItem_t> idChildren;     // список индексов дочерних элементов
+    idDataBaseItem_t idParent;                // Индекс родителя
+
+    void init(idDataBaseItem_t id, idDataBaseItem_t idP, isRoot_t isR);
 };
 
 #endif // DATABASEITEM_H
