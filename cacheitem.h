3168614c8fc6143cbc7655b027aaa34fe148c119
Dmitriy <dn.rybakov@ddkmed.ru>
2019-08-07 17:21:05 +0700

    Описал новую структуру полей элемента кэша

--- cacheitem.h
+++ cacheitem.h
@@ -3,22 +3,24 @@
 
 #include "databaseitem.h"
 #include "QVector"
+#include "QString"
 
-class CacheItem : public DataBaseItem
+class CacheItem
 {
 public:
     CacheItem();
     CacheItem(CacheItem *parent);
-    //CashItem(DataBaseItem dbItem);
 
     void addChild(CacheItem *child);
     int getNumChildren(void) {return children.size();}
     CacheItem* getChild(int i) {return children.at(i);}
 private:
-    bool isNew;                 // Признак нового элемента в кэше
-
-    CacheItem *Parent;
-    QVector<CacheItem*>children;
+    bool isNew;                     // Признак нового элемента в кэше
+    idDataBaseItem_t id;            // Уникальный ID элемента в БД (для перенесенных из БД)
+    CacheItem *Parent;              // Указатель на родителя в кэше
+    isRoot_t isRoot;                // Признак корня
+    QVector<CacheItem*>children;    // Указатели на дочерние в кеше
+    QString value;
 };
 
 #endif // CASHITEM_H
